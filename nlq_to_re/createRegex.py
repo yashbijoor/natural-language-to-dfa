8df8051c7b7954a74caf50a2c891cda727f86f68
yashbijoor <yashbijoor2002@gmail.com>
2023-11-26 19:23:06 +0530

    created requirements.txt

--- nlq_to_re/createRegex.py
+++ nlq_to_re/createRegex.py
@@ -7,8 +7,8 @@ def construct_regex(charset, char_dictionary):
         regexOutput.oneOfTheCharacters(char_dictionary['start']).anyNumberOf(charset)
     if 'contain' in char_dictionary:
         for ch in char_dictionary['contain']:
-            regexOutput.exactString(ch).anyNumberOf(charset)
+            regexOutput.anyNumberOf(charset).exactString(ch).anyNumberOf(charset)
     if 'end' in char_dictionary:
-        regexOutput.oneOfTheCharacters(char_dictionary['end'])
+        regexOutput.anyNumberOf(charset).oneOfTheCharacters(char_dictionary['end'])
 
     return regexOutput.regex
