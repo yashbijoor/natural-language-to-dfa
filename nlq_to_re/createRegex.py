3b8058ef7c9a628e3cf9702de92508144139f6a3
yashbijoor <yashbijoor2002@gmail.com>
2024-03-06 22:31:21 +0530

    added odd/even cases, fixed visualization err

--- nlq_to_re/createRegex.py
+++ nlq_to_re/createRegex.py
@@ -16,11 +16,21 @@ def construct_regex(charset, char_dictionary):
                 regexOutput.atmost(temp[-2:])
                 charset_modified.append(temp[-1])
                 case_no = 2
+            elif 'even' in i:
+                temp = i.split('_')
+                regexOutput.even(temp[-1])
+                charset_modified.append(temp[-1])
+                case_no = 3
+            elif 'odd' in i:
+                temp = i.split('_')
+                regexOutput.odd(temp[-1])
+                charset_modified.append(temp[-1])
+                case_no = 4
             else:
                 temp = i.split('_')
                 regexOutput.specificNumberOf(temp[-2:])
                 charset_modified.append(temp[-1])
-                case_no = 3
+                case_no = 5
         regexOutput.oneOfTheCharacters(regexOutput.tempList)
         regexOutput.compileRegex()
         if case_no == 1:
@@ -28,22 +38,62 @@ def construct_regex(charset, char_dictionary):
         else:
             regexOutput.anyNumberOf(list(set(charset) - set(charset_modified)))
         regexOutput.compileRegex()
+        regexOutput.clearTempList()
 
 
     if 'contain' in char_dictionary:
+        case_no = 0
+        charset_modified = []
         for ch in char_dictionary['contain']:
             if 'atleast' in ch:
                 temp = ch.split('_')
+                charset_modified.append(temp[-1])
+                case_no = 1
                 regexOutput.anyNumberOf(charset).exactString(temp[-1]).anyNumberOf(charset)
             elif 'atmost' in ch:
                 temp = ch.split('_')
+                charset_modified.append(temp[-1])
+                case_no = 2
                 regexOutput.anyNumberOf(charset).exactString(temp[-1]).anyNumberOf(charset)
+            elif 'even' in ch:
+                temp = ch.split('_')
+                charset_modified.append(temp[-1])
+                case_no = 3
+                ch_even_odd = temp[-1]
+            elif 'odd' in ch:
+                temp = ch.split('_')
+                charset_modified.append(temp[-1])
+                case_no = 4
+                ch_even_odd = temp[-1]
             else:
                 temp = ch.split('_')
+                charset_modified.append(temp[-1])
+                case_no = 5
                 regexOutput.anyNumberOf(charset).exactString(temp[-1]).anyNumberOf(charset)
-        regexOutput.concat(regexOutput.tempList)
-        regexOutput.compileRegex()
-
+        # if case_no == 1:
+        #     regexOutput.anyNumberOf(charset)
+        # elif case_no == 3:
+        #     regexOutput.exactString
+        # else:
+        #     regexOutput.anyNumberOf(list(set(charset) - set(charset_modified)))
+        if case_no == 3:
+            regexOutput.anyNumberOf(list(set(charset) - set(charset_modified))).exactString(ch_even_odd).anyNumberOf(list(set(charset) - set(charset_modified))).exactString(ch_even_odd).anyNumberOf(list(set(charset) - set(charset_modified)))
+            regexOutput.concat(regexOutput.tempList)
+            regexOutput.loop(regexOutput.tempList[-1])
+            regexOutput.compileRegex()
+            regexOutput.clearTempList()
+        elif case_no == 4:
+            regexOutput.anyNumberOf(list(set(charset) - set(charset_modified))).exactString(ch_even_odd).anyNumberOf(list(set(charset) - set(charset_modified))).exactString(ch_even_odd).anyNumberOf(list(set(charset) - set(charset_modified)))
+            regexOutput.concat(regexOutput.tempList)
+            regexOutput.loop(regexOutput.tempList[-1])
+            regexOutput.exactString(ch_even_odd)
+            regexOutput.concat(regexOutput.tempList[-2:])
+            regexOutput.compileRegex()
+            regexOutput.clearTempList()
+        else:
+            regexOutput.concat(regexOutput.tempList)
+            regexOutput.compileRegex()
+            regexOutput.clearTempList()
 
 
     if 'end' in char_dictionary:
@@ -60,10 +110,20 @@ def construct_regex(charset, char_dictionary):
                 charset_modified.append(temp[-1])
                 case_no = 2
                 regexOutput.atmost(temp[-2:])
+            elif 'even' in i:
+                temp = i.split('_')
+                regexOutput.even(temp[-1])
+                charset_modified.append(temp[-1])
+                case_no = 3
+            elif 'odd' in i:
+                temp = i.split('_')
+                regexOutput.odd(temp[-1])
+                charset_modified.append(temp[-1])
+                case_no = 4
             else:
                 temp = i.split('_')
                 charset_modified.append(temp[-1])
-                case_no = 3
+                case_no = 5
                 regexOutput.specificNumberOf(temp[-2:])
         if case_no == 1:
             regexOutput.anyNumberOf(charset)
@@ -73,5 +133,6 @@ def construct_regex(charset, char_dictionary):
         regexOutput.clearTempListPartially(1)
         regexOutput.oneOfTheCharacters(regexOutput.tempList)
         regexOutput.compileRegex()
+        regexOutput.clearTempList()
 
     return regexOutput.regex
